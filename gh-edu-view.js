98d97f32017b795bdfac7ea35a61f277e9f78ad9
GGCristo <cgg.code@gmail.com>
2022-05-07 18:30:47 +0100

    First test

--- gh-edu-view.js
+++ gh-edu-view.js
@@ -1,1 +1,17 @@
-console.log("Probando");
+import { program } from 'commander'
+/** _dirname doesnt work with modules */
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+/***/
+// import { config } from __dirname + '/../'
+
+console.log(__dirname);
+program
+    .command("users")
+    .action(() => {
+      console.log("Hola");
+    })
+program.parse();
